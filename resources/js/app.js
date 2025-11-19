2c45adbbdc6723d008fa42a4785fc024f185d444
barsheel <barsheel@gmail.com>
2025-11-19 22:18:56 +0700

    install rails/ujs

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,9 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import ujs from '@rails/ujs'
+
+document.addEventListener("DOMContentLoaded", () => {
+    ujs.start()
+});
