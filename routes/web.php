9805aca2f84cc68cfe1e704b4130438d6cdeb015
barsheel <barsheel@gmail.com>
2025-11-27 20:21:05 +0700

    fix Routes for task_statuses resource

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,12 @@ use App\Http\Controllers\TaskStatusController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::resource('task_statuses', TaskStatusController::class);
+Route::resource('task_statuses', TaskStatusController::class)
+    ->except(['index','show'])
+    ->middleware('auth');
+
+Route::resource('task_statuses', TaskStatusController::class)
+    ->only(['index', 'show']);
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
 });
 
 require __DIR__.'/auth.php';
