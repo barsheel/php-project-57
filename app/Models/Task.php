75146c4f076a596b397975b693172f2f752700f5
barsheel <barsheel@gmail.com>
2025-12-22 18:16:52 +0700

    create Task Model

---
+++ app/Models/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'status_id',
+        'assigned_to_id',
+        'created_by_id'
+    ];
+
+    public function status()
+    {
+        return $this->belongsTo(TaskStatus::class, 'status_id');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by_id');
+    }
+
+    public function assigned()
+    {
+        return $this->belongsTo(User::class, 'assigned_to_id');
+    }
+}
