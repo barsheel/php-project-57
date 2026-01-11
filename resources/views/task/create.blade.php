93bcdd3428aa391224b7f660fd0402a790b0f8e2
barsheel <barsheel@gmail.com>
2026-01-11 19:58:53 +0700

    fix task.create view

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -53,11 +53,7 @@
                         <select id="labels[]" name="labels[]" multiple=""
                                 class="mt-1 block border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
                             @foreach($labels as $label)
-                                @if($task->labels()->where('label_id', $label->id)->exists())
-                                    <option selected="selected" value="{{ $label->id }}">{{ $label->name }}</option>
-                                @else
-                                    <option value="{{ $label->id }}">{{ $label->name }}</option>
-                                @endif
+                                <option value="{{ $label->id }}">{{ $label->name }}</option>
                             @endforeach
                         </select>
                         <x-input-error class="mt-2" :messages="$errors->get('labels')"/>
