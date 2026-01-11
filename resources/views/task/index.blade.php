86c67b6cf26d840fb5620cd6c9300540498de544
barsheel <barsheel@gmail.com>
2026-01-11 19:59:10 +0700

    add pagination for Tasks

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -103,9 +103,9 @@
                 </tbody>
             </table>
 
-            {{--<div class="mt-4">
+            <div class="mt-4">
                 {{ $tasks->links() }}
-            </div>--}}
+            </div>
         </div>
     </div>
 </x-app-layout>
