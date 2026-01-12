918676c1bac5366e1d59b9b8bc91c528ea2da2a6
barsheel <barsheel@gmail.com>
2026-01-13 00:55:50 +0700

    remove translation keys with hardcoded names in auth.register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Имя')"/>
+            <x-input-label for="name" value="Имя"/>
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
                           autofocus autocomplete="name"/>
             <x-input-error :messages="$errors->get('name')" class="mt-2"/>
@@ -12,7 +12,7 @@
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')"/>
+            <x-input-label for="email" value='Email'/>
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
                           autocomplete="username"/>
             <x-input-error :messages="$errors->get('email')" class="mt-2"/>
@@ -20,7 +20,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Пароль')"/>
+            <x-input-label for="password" value="Пароль"/>
 
             <x-text-input id="password" class="block mt-1 w-full"
                           type="password"
@@ -32,7 +32,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Подтверждение')"/>
+            <x-input-label for="password_confirmation" value="Подтверждение"/>
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                           type="password"
@@ -44,12 +44,10 @@
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                href="{{ route('login') }}">
-                {{ __('Уже зарегистрированы?') }}
+                Уже зарегистрированы?
             </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Зарегистрировать') }}
-            </x-primary-button>
+            <x-primary-button class="ms-4">Зарегистрировать</x-primary-button>
         </div>
     </form>
 </x-guest-layout>
