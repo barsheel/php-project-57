119c0be91f32f119a013ea2d709559cdb1e7282f
barsheel <barsheel@gmail.com>
2026-01-14 23:50:27 +0700

    replace x-primary-button with hardcode button with correct styles

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -50,7 +50,9 @@
             {{--<button type="submit">
                 Зарегистрировать
             </button>--}}
-            <x-primary-button class="ms-4">Зарегистрировать</x-primary-button>
+            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ms-4">
+                Зарегистрировать
+            </button>
         </div>
     </form>
 </x-guest-layout>
