cb77f22e6f2143b77327881b7db8953db909c0a2
barsheel <barsheel@gmail.com>
2026-01-15 00:55:08 +0700

    create general message in x-input-error component

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,6 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
+    <p {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>Упс! Что-то пошло не так:</p>
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
