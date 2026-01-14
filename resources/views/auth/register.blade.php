f4b63c89527131a95f6ff2aae4bcfdc73e55dc59
barsheel <barsheel@gmail.com>
2026-01-15 00:55:55 +0700

    move registration error output in one place

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,12 +2,15 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
+        <x-input-error :messages="$errors->all()" class="mt-2"/>
+
+
         <!-- Name -->
         <div>
             <x-input-label for="name" value="Имя"/>
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
                           autofocus autocomplete="name"/>
-            <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+
         </div>
 
         <!-- Email Address -->
@@ -15,7 +18,6 @@
             <x-input-label for="email" value='Email'/>
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
                           autocomplete="username"/>
-            <x-input-error :messages="$errors->get('email')" class="mt-2"/>
         </div>
 
         <!-- Password -->
@@ -26,8 +28,6 @@
                           type="password"
                           name="password"
                           required autocomplete="new-password"/>
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2"/>
         </div>
 
         <!-- Confirm Password -->
@@ -37,8 +37,6 @@
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                           type="password"
                           name="password_confirmation" required autocomplete="new-password"/>
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2"/>
         </div>
 
         <div class="flex items-center justify-end mt-4">
@@ -47,9 +45,6 @@
                 Уже зарегистрированы?
             </a>
 
-            {{--<button type="submit">
-                Зарегистрировать
-            </button>--}}
             <x-primary-button class="ms-4">Зарегистрировать</x-primary-button>
         </div>
     </form>
