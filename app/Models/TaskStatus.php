d9cf24a61d3527a0091149258c3ed46bb6893cc7
barsheel <barsheel@gmail.com>
2026-01-20 00:06:50 +0700

    fix tasks relation in TaskStatus model

--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -14,6 +14,6 @@ class TaskStatus extends Model
 
     public function tasks()
     {
-        return $this->hasMany(Task::class);
+        return $this->hasMany(Task::class, 'status_id');
     }
 }
